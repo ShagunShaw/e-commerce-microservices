244d4618a43c63161adec04f517db38c76d17ca2
Shagun Shaw <shagunshaw2006@gmail.com>
2026-03-25 23:28:08 +0530

    Update code

--- apps/catalog/src/main.ts
+++ apps/catalog/src/main.ts
@@ -1,8 +1,38 @@
 import { NestFactory } from '@nestjs/core';
 import { CatalogModule } from './catalog.module';
+import { Logger } from '@nestjs/common';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(CatalogModule);
-  await app.listen(process.env.port ?? 3000);
+  process.title = 'catalog'
+
+  const logger = new Logger('CatalogBootstrap')
+
+  // const port= Number(process.env.CATALOG_TCP_PORT ?? 4011)
+
+  const rmq_url= `${process.env.RABBITMQ_URL}` || 'amqp://loclhost:5672'
+
+  const queue= `${process.env.CATALOG_QUEUE}` || 'catalog_queue'
+
+  // create a microservice instance
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    CatalogModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [rmq_url],
+        queue,
+        queueOptions: {
+          durable: (process.env.PRODUCTION)?true:false  
+        },
+      }
+    },
+  );
+
+  app.enableShutdownHooks()
+
+  await app.listen()
+
+  logger.log(`Catalog RMQ listening on queue ${queue} via ${rmq_url}`)
 }
 bootstrap();
--- apps/media/src/main.ts
+++ apps/media/src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { MediaModule } from './media.module';
+import { Logger } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(MediaModule);
-  await app.listen(process.env.port ?? 3000);
+  process.title = 'media';
+
+  const logger= new Logger('MediaBootstrap')
+
+  // const port= Number(process.env.MEDIA_TCP_PORT ?? 4013)
+
+  const rmq_url= `${process.env.RABBITMQ_URL}` || 'amqp://loclhost:5672'
+
+  const queue= `${process.env.MEDIA_QUEUE}` || 'media_queue'
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    MediaModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [rmq_url],
+        queue,
+        queueOptions: {
+          durable: (process.env.PRODUCTION)?true:false 
+        },
+      }
+    },
+  );
+
+  app.enableShutdownHooks()
+  await app.listen();
+
+  logger.log(`Media RMQ listening on queue ${queue} via ${rmq_url}`)
 }
 bootstrap();
